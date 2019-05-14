453e87ca266899146d85c576378eeeba900ed433
yanqixuan <1402412256@qq.com>
2019-05-14 10:43:34 +0800

    秒杀栏完成

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -21,6 +21,37 @@ Page({
         img:'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
         text:'手办大本营'
       }
-    ]
+    ],
+
+    msTitle:[
+      {
+        time:'7:00',
+        img:[
+              'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+              'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+              'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+              'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+            ]
+      },
+      {
+        time:'13:00',
+        img:[
+          'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+          'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+          'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+          'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+        ]
+      },
+      {
+        time:'20:00',
+        img:[
+          'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+          'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640',
+          'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+          'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+          
+        ]
+      }
+    ],
   }
 })
