56aaee33525e8da0690afab81c96420d7267cdf7
yanqixuan <1402412256@qq.com>
2019-05-18 20:09:27 +0800

    group交互

--- miniprogram/pages/group/index.js
+++ miniprogram/pages/group/index.js
@@ -5,183 +5,233 @@ Page({
    * 页面的初始数据
    */
   data: {
-    leftList:[
+    toView: "",
+    curId: "  ",
+    leftList: [
       {
-        name:'',
-        list:[
+        name: '',
+        select: true,
+        list: [
           {
-            title:"推荐",
-            id:"tuijian"
+            title: "推荐",
+            id: "tuijian"
           }
         ]
       },
       {
-        name:"作品",
-        list:[
+        name: "作品",
+        select: false,
+        list: [
           {
-            title:"影视",
-            id:"yinshi"
+            title: "影视",
+            id: "yinshi"
           },
           {
-            title:"形象",
-            id:"xingxiang"
+            title: "形象",
+            id: "xingxiang"
           },
           {
-            title:"动漫",
-            id:"dongman"
+            title: "动漫",
+            id: "dongman"
           },
           {
-            title:"文化",
-            id:"wenhua"
+            title: "文化",
+            id: "wenhua"
           },
           {
-            title:"潮流/综艺",
-            id:"chaoliu"
+            title: "潮流/综艺",
+            id: "chaoliu"
           },
           {
-            title:"游戏",
-            id:"youxi"
+            title: "游戏",
+            id: "youxi"
           }
         ]
       },
       {
-        name:"商品",
-        list:[
+        name: "商品",
+        select: false,
+        list: [
           {
-            title:"男女服饰",
-            id:"nannvfushi"
+            title: "男女服饰",
+            id: "nannvfushi"
           },
           {
-            title:"数码配件",
-            id:"shumapeijian"
+            title: "数码配件",
+            id: "shumapeijian"
           },
           {
-            title:"生活家居",
-            id:"shenghuojiaju"
+            title: "生活家居",
+            id: "shenghuojiaju"
           },
           {
-            title:"鞋帽配饰",
-            id:"xiemaopeoshi"
+            title: "鞋帽配饰",
+            id: "xiemaopeoshi"
           },
           {
-            title:"玩具手办",
-            id:"wanjushouban"
+            title: "玩具手办",
+            id: "wanjushouban"
           },
           {
-            title:"箱包收纳",
-            id:"xiangbaoshouna"
+            title: "箱包收纳",
+            id: "xiangbaoshouna"
           },
           {
-            title:"首饰配件",
-            id:"shoushipeijian"
+            title: "首饰配件",
+            id: "shoushipeijian"
           },
           {
-            title:"美妆个护",
-            id:"meizhuanggehu"
+            title: "美妆个护",
+            id: "meizhuanggehu"
           },
           {
-            title:"休闲食品",
-            id:"xiuxianshipin"
+            title: "休闲食品",
+            id: "xiuxianshipin"
           },
           {
-            title:"文体图书",
-            id:"wentitushu"
+            title: "文体图书",
+            id: "wentitushu"
           },
 
         ]
       }
     ],
-    rightList:[
+    rightList: [
       {
-        title:"独家正品",
-        list:[
+        title: "—— 独家正品 ——",
+        id: "tuijian",
+        isProduct: false,
+        list: [
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           }
         ]
       },
       {
-        title:"人气类目",
-        list:[
+        title: "—— 人气类目 ——",
+        id: "人气类目",
+        isProduct: true,
+        list: [
           {
-            img:'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
-            name:'背包'
+            img: 'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
+            name: '背包'
           },
           {
-            img:'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
-            name:'背包'
+            img: 'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
+            name: '背包'
           },
           {
-            img:'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
-            name:'背包'
+            img: 'http://imgcache.gtimg.cn/ACT/svip_act/act_img/public/201809/1535964204_bag.png',
+            name: '背包'
           },
           {
-            img:'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
-            name:'U形枕'
+            img: 'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
+            name: 'U形枕'
           },
           {
-            img:'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
-            name:'U形枕'
+            img: 'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
+            name: 'U形枕'
           },
           {
-            img:'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
-            name:'U形枕'
+            img: 'https://p.qpic.cn/qqumall/0/15542058905ca34cc243426.png/0',
+            name: 'U形枕'
           },
         ]
       },
       {
-        title:"影视",
-        list:[
+        title: "—— 影视 ——",
+        id: "yinshi",
+        list: [
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           },
           {
-            img:'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
-            name:'怪奇鹅'
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
+          },
+          {
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
+          },
+          {
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
+          },
+          {
+            img: 'http://p.qpic.cn/qqumall/0/15244568995add5dc3a1f4b.png/0',
+            name: '怪奇鹅'
           }
         ]
       }
     ]
   },
+  switch(event) {
+    // console.log(event);
+    this.setData({
+      toView: event.target.dataset.id,
+      curId: event.target.dataset.id
+    })
+    // console.log(this.data.curId,event.target.dataset.id,this.data.leftList);
+  },
+  scrollTab(event) {
+    console.log(event);
+    let st = event.detail.scrollTop
+    console.log(st)
+    switch (st) {
+      case st > 396:
+        this.setData({
+          curId: "yinshi"
+        })
+        console.log(this.data.curId)
+        break;
+      case st >= 0:
+        this.setData({
+          curId: "tuijian"
+        })
+        break;
 
+      default:
+        break;
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
---
+++ spider/index.js
@@ -0,0 +1,6 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const cheerid = require('cheerio')
+const url = 'https://umall.vip.qq.com/v2/?adtag=share&_wv=1025&_wvx=10&_wwv=4&path=/home&hasRedDot=false&query=adtag2%253Dfl_zp&_nav_alpha=0#/home?adtag2=fl_zp';
+const imageDir = './images/'
