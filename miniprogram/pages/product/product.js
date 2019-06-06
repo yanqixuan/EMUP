112c3566c6f7c8fcf7f41701c73dcf311803bd08
yanqixuan <1402412256@qq.com>
2019-06-06 19:27:22 +0800

    product data

--- miniprogram/pages/product/product.js
+++ miniprogram/pages/product/product.js
@@ -1,6 +1,6 @@
 // miniprogram/pages/product/product.js
 const db = wx.cloud.database({
-  env:'http-product'
+  env: 'http-product'
 })
 Page({
 
@@ -8,8 +8,50 @@ Page({
    * 页面的初始数据
    */
   data: {
+    
+        id: '1',
+        img: [
+          'http://p.qpic.cn/qqumall/0/15373333205ba1d8486749e.jpg/slt01',
+          '//p.qpic.cn/qqumall/0/15373333455ba1d861bfdc5.jpg/slt01',
+          '//p.qpic.cn/qqumall/0/15373333615ba1d8713abe8.jpg/slt01'
+        ],
+        name: '歪瓜出品6包100抽本色抽纸家用纸巾',
+        num: '3.9万人已付款',
+        price: '9.99',
+        oldPrice: '19.80',
+        info: {
+          postage: '包邮(部分地区)',
+          type: '6包/提 1个',
+          server: [
+            '正版授权', '7天退换', '批量抽检'
+          ]
+        },
+        shop:{
+          img:'https://p.qpic.cn/qqumall/0/15280955515b14e33f40ce5.png/0',
+          name:'歪瓜出品旗舰店',
+          introduce:'歪瓜出品 必属精品'
+        },
+        evaluate:{
+          num:'26453',
+          percent:'98.00%'
+        },
+        promise:{
+          img:'//imgcache.gtimg.cn/ACT/svip_act/act_img/public/201808/m1535464484_guarantee-sell.png'
+        },
+        details:[
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab2c290.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab1e6ff.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab17ee8.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab2050e.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab250fc.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab34047.jpg/0',
+          '//p.qpic.cn/qqumall/0/15373331635ba1d7ab364a7.jpg/0',
+          '//p.qpic.cn/qqumall/0/15453596065c1c50f6f1128.jpg/0'
+        ]
 
-  },
+      
+  
+},
 
   /**
    * 生命周期函数--监听页面加载
@@ -20,14 +62,14 @@ Page({
     // console.log(options.id)
     let that = this;
     db.collection('productLIst').where({
-      id:options.id
+      id: options.id
     })
-    // // console.log(productList);
-    .get({
-      success(res){
-        console.log(res.data)
-      }
-    })
+      // // console.log(productList);
+      .get({
+        success(res) {
+          console.log(res.data)
+        }
+      })
   },
 
   /**
--- spider/index.js
+++ spider/index.js
@@ -3,7 +3,8 @@
 // const path = require('path');
 const request = require('request')
 const cheerid = require('cheerio')
-const url = 'https://umall.vip.qq.com/v2/?adtag=share&_wv=1025&_wvx=10&_wwv=4&path=/home&hasRedDot=false&query=adtag2%253Dfl_zp&_nav_alpha=0#/home?adtag2=fl_zp';
+// const url = 'https://umall.vip.qq.com/v2/?adtag=share&_wv=1025&_wvx=10&_wwv=4&path=/home&hasRedDot=false&query=adtag2%253Dfl_zp&_nav_alpha=0#/home?adtag2=fl_zp';
+const url ='https://umall.vip.qq.com/v2/?adtag=share&_wv=1025&_wvx=10&_wwv=4&path=/product/38715&hasRedDot=false&query=adtag2%253Dsy_goodsjxtjV5&_nav_alpha=0#/product/38715?adtag2=sy_goodsjxtjV5'
 // const imageDir = './images/'
 request(url,(err,res)=>{
     if(!err){
