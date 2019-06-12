d6bcc059373017db3ab5e92eedea0a8ad410cbfd
yanqixuan <1402412256@qq.com>
2019-06-12 21:51:48 +0800

    groupList

---
+++ miniprogram/pages/groupList/groupList.js
@@ -0,0 +1,90 @@
+// miniprogram/pages/groupList/groupList.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img:"",
+    title:'',
+    introduce:'',
+    productList:[]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(options.pid)
+    let pid = options.pid
+    wx.request({
+      url: 'https://www.easy-mock.com/mock/5ce49f32d66f474c5b3a024e/emup/'+pid,
+      data: {},
+      header: {'content-type':'application/json'},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: (result)=>{
+        let res = result.data.data;
+        // console.log(result.data.data)
+        this.setData({
+          img:res.img,
+          title:res.title,
+          introduce:res.introduce,
+          productList:res.productList
+        })
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
