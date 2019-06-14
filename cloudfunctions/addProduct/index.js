9c9b8a0f8652a30ba68f36ce45a041dfc365527d
yanqixuan <1402412256@qq.com>
2019-06-14 21:55:07 +0800

    addProduct function complete

--- cloudfunctions/addProduct/index.js
+++ cloudfunctions/addProduct/index.js
@@ -3,35 +3,56 @@ const cloud = require('wx-server-sdk')
 
 cloud.init()
 const db = cloud.database({
-  env:'http-product'
+  env: 'http-product'
 })
 // 云函数入口函数
 exports.main = async (event, context) => {
-  console.log("111111111111111111111",event)
   const wxContext = cloud.getWXContext()
-  const __id=event._id;
+  const __id = event._id;
   let res = await db.collection('productList').where({
-    _id:__id
+    _id: __id
   }).get()
-  const name = res.data[0].name;
-  const type = res.data[0].info.type;
-  const price = res.data[0].price;
-  const title = res.data[0].info.shop.name;
 
-  let resAdd = await db.collection('cart').add({
-    data:{
-      _id:__id,
+  let cart = await db.collection('cart')
+
+  let resSelect = await cart.where({
+    _id: __id
+  }).get()
+  if (resSelect.data.length === 0) {
+    const name = res.data[0].name;
+    const type = res.data[0].info.type;
+    const price = res.data[0].price;
+    const title = res.data[0].info.shop.name;
+
+    // let resAdd = 
+    await cart.add({
+    data: {
+      _id: __id,
       name,
       type,
       price,
-      title
+      title,
+      num: 1
     }
-  })
+    })
+
+  } else {
+    await cart.where({
+      _id: __id
+    }).update({
+      data: {
+        num: resSelect.data[0].num + 1
+      }
+    })
+  }
+
+
 
   return {
-    resAdd,
-    res,
-    // __id,
+    // resAdd,
+    // resSelect,
+    // res,
+    __id,
     event,
     openid: wxContext.OPENID,
     appid: wxContext.APPID,
